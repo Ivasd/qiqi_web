c4c5ca014c8f81a879bd6de8b0404ad03190e381
Ivasd <66891803+Ivasd@users.noreply.github.com>
2025-08-19 17:03:15 +0000

    Deploying to gh-pages from @ Ivasd/qiqi_web@656e919b768f87c7d310e1ea45b733c507445ed6 🚀

--- _scripts/dark-mode.js
+++ _scripts/dark-mode.js
@@ -1,22 +1,22 @@
 /*
-  manages light/dark mode.
+  管理深色/浅色模式，默认深色模式
 */
 
 {
-  // immediately load saved (or default) mode before page renders
+  // 页面渲染前加载保存的模式（默认深色模式）
   document.documentElement.dataset.dark =
-    window.localStorage.getItem("dark-mode") ?? "false";
+    window.localStorage.getItem("dark-mode") ?? "true"; // 这里将默认值改为 "true"
 
   const onLoad = () => {
-    // update toggle button to match loaded mode
+    // 更新切换按钮状态以匹配加载的模式
     document.querySelector(".dark-toggle").checked =
       document.documentElement.dataset.dark === "true";
   };
 
-  // after page loads
+  // 页面加载后执行
   window.addEventListener("load", onLoad);
 
-  // when user toggles mode button
+  // 当用户切换模式按钮时
   window.onDarkToggleChange = (event) => {
     const value = event.target.checked;
     document.documentElement.dataset.dark = value;
